ed630aeb857fe66878f9bc5eea543cc55fbd7bd0
oracle <oracle-main@pm.me>
2023-05-09 21:17:38 +0100

    Add program info header

---
+++ include/prog_info.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+
+namespace tropical {
+
+inline constexpr std::uint16_t version_major = 0;
+inline constexpr std::uint16_t version_minor = 1;
+inline constexpr std::uint16_t version_patch = 0;
+
+} // namespace tropical
