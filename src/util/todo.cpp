e70f07c1c38134be0242065a394b1ef13bb8b684
oracle <oracle-main@pm.me>
2023-05-10 07:04:13 +0100

    Add pretty printing for todo

--- src/util/todo.cpp
+++ src/util/todo.cpp
@@ -2,16 +2,19 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <fmt/color.h>
 #include <fmt/core.h>
 
 [[noreturn]]
 void todo(std::source_location const loc) {
     fmt::print(
-            stderr,
-            "unimplemented '{}' at {}:{}\n",
-            loc.function_name(),
-            loc.file_name(),
-            loc.line()
+        stderr,
+        "{} {}\n"
+        "at {}:{}\n",
+        fmt::styled("unimplemented function", fmt::fg(fmt::color::red)),
+        loc.function_name(),
+        loc.file_name(),
+        loc.line()
     );
     std::fflush(stderr);
     std::abort();
