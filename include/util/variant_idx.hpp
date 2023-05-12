5f8556f18339f4a4240b1e19d607d9aa44e9176a
oracle <oracle-main@pm.me>
2023-05-12 23:28:38 +0100

    Add variant_idx utility

---
+++ include/util/variant_idx.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstddef>
+#include <variant>
+
+template <typename VariantType, typename T, std::size_t idx = 0>
+consteval std::size_t variant_idx() {
+    static_assert(
+        idx < std::variant_size_v<VariantType>,
+        "Type not found in variant"
+    );
+
+    if constexpr (idx == std::variant_size_v<VariantType>) {
+        return idx;
+    } else if constexpr (std::is_same_v<
+                             std::variant_alternative_t<idx, VariantType>,
+                             T>) {
+        return idx;
+    } else {
+        return variant_idx<VariantType, T, idx + 1>();
+    }
+}
