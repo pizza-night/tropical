4aae6bb9307bf25dd5aaf7b0f3e01de29ed0210f
oracle <oracle-main@pm.me>
2023-05-12 04:51:35 +0100

    Add RAII socket utility

---
+++ include/util/socket.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <system_error>
+
+class Socket {
+  private:
+    int m_fd;
+
+    explicit Socket(int) noexcept;
+
+  public:
+    Socket(Socket const&) = delete;
+    Socket(Socket&&) noexcept;
+
+    Socket& operator=(Socket const&) = delete;
+    Socket& operator=(Socket&&) noexcept;
+
+    ~Socket() noexcept;
+
+    int fd() noexcept;
+
+    bool open() const noexcept;
+
+    std::error_code manually_drop() noexcept;
+
+    static Socket from_fd(int) noexcept;
+};
---
+++ src/util/socket.cpp
@@ -0,0 +1,43 @@
+#include "util/socket.hpp"
+
+#include <unistd.h>
+
+Socket::Socket(int const fd) noexcept : m_fd(fd) {}
+
+Socket::Socket(Socket&& other) noexcept : m_fd(other.m_fd) {
+    other.m_fd = -1;
+}
+
+Socket& Socket::operator=(Socket&& other) noexcept {
+    if (this != &other) {
+        close(m_fd);
+        m_fd = other.m_fd;
+        other.m_fd = -1;
+    }
+    return *this;
+}
+
+Socket::~Socket() noexcept {
+    if (m_fd != -1) {
+        close(m_fd);
+    }
+}
+
+int Socket::fd() noexcept {
+    return m_fd;
+}
+
+bool Socket::open() const noexcept {
+    return m_fd != -1;
+}
+
+std::error_code Socket::manually_drop() noexcept {
+    return std::error_code(
+        (m_fd != -1 and close(m_fd) == -1) ? errno : 0,
+        std::system_category()
+    );
+}
+
+Socket Socket::from_fd(int const fd) noexcept {
+    return Socket(fd);
+}
