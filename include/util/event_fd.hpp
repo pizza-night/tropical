f8be67994f3010a8f4d0673df8f5ae59d14485b7
oracle <oracle-main@pm.me>
2023-05-17 06:15:47 +0100

    Implement proper RAII for eventfd wrapper

--- include/util/event_fd.hpp
+++ include/util/event_fd.hpp
@@ -1,12 +1,21 @@
 #pragma once
 
+#include <system_error>
+
 class EventFd {
   private:
     int m_fd;
 
   public:
     EventFd() noexcept;
+    EventFd(EventFd const&) = delete;
+    EventFd(EventFd&&) noexcept;
+    EventFd& operator=(EventFd const&) = delete;
+    EventFd& operator=(EventFd&&) noexcept;
     ~EventFd();
+
+    [[nodiscard]]
+    std::error_code manually_close() noexcept;
 
     void mark_write() noexcept;
     void mark_read() noexcept;
--- src/util/event_fd.cpp
+++ src/util/event_fd.cpp
@@ -7,8 +7,31 @@ EventFd::EventFd() noexcept {
     m_fd = eventfd(0, EFD_CLOEXEC | EFD_SEMAPHORE);
 }
 
+EventFd::EventFd(EventFd&& other) noexcept : m_fd(other.m_fd) {
+    other.m_fd = -1;
+}
+
+EventFd& EventFd::operator=(EventFd&& other) noexcept {
+    if (this != &other) {
+        close(m_fd);
+        m_fd = other.m_fd;
+        other.m_fd = -1;
+    }
+    return *this;
+}
+
 EventFd::~EventFd() {
-    close(m_fd);
+    if (m_fd != -1) {
+        close(m_fd);
+    }
+}
+
+[[nodiscard]]
+std::error_code EventFd::manually_close() noexcept {
+    return std::error_code(
+        (m_fd != -1 and close(m_fd) == -1) ? errno : 0,
+        std::system_category()
+    );
 }
 
 void EventFd::mark_write() noexcept {
