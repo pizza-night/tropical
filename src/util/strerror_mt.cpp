44c92e69e80783c3d5ea2a3d667b9d9659f25bd7
oracle <oracle-main@pm.me>
2023-05-19 21:06:50 +0100

    Readable strerror_mt

--- src/util/strerror_mt.cpp
+++ src/util/strerror_mt.cpp
@@ -6,11 +6,13 @@
 [[nodiscard]]
 char const* strerror_mt(int errnum) noexcept {
     static thread_local char buf[256];
-    return strerror_r(errnum, buf, std::size(buf))
+    auto res = strerror_r(errnum, buf, std::size(buf));
+
 #if _POSIX_C_SOURCE >= 200'112L and not _GNU_SOURCE
-           // POSIX version of strerror_r returns an int.
-           ,
-           buf
+    // POSIX version of strerror_r returns an int, which we ignore.
+    return buf;
+#else
+    // GNU version of strerror_r returns a char*, which we return.
+    return res;
 #endif
-            ;
 }
